98c7bb0629182bb470e14dfe1ff79fb264fa38f7
Harry <harrywendson@gmail.com>
2022-10-22 22:25:31 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/harrywendson/apiweb/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.harrywendson.apiweb.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
