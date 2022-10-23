55d4b25cbc3b1e53552b42bb509854fb99f71558
Harry <harrywendson@gmail.com>
2022-10-23 14:11:21 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/harrywendson/apiweb/config/Instantiation.java
+++ src/main/java/com/harrywendson/apiweb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.harrywendson.apiweb.domain.Post;
 import com.harrywendson.apiweb.domain.User;
 import com.harrywendson.apiweb.dto.AuthorDTO;
+import com.harrywendson.apiweb.dto.CommentDTO;
 import com.harrywendson.apiweb.repository.PostRepository;
 import com.harrywendson.apiweb.repository.UserRepository;
 
@@ -39,6 +40,14 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para PHO. Abraços!", new AuthorDTO(key));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia", "Acordei feliz hoje!", new AuthorDTO(key));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem bb!", sdf.parse("21/03/2018"), new AuthorDTO(raquel));
+		CommentDTO c2 = new CommentDTO("Quem deixou!", sdf.parse("22/03/2018"), new AuthorDTO(harry));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(harry));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		key.getPosts().addAll(Arrays.asList(post1,post2));
